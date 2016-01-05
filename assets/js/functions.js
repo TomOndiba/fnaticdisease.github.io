758145c47d20fceae245c1e4380d8a362992f4dc
Radulescu Petru <tuty.radulescu@yahoo.com>
2016-01-05 15:51:14 +0200

    added all files

---
+++ assets/js/functions.js
@@ -0,0 +1,62 @@
+
+$( document ).ready(function() {
+    
+    
+    $(".more").on("click",function(){
+      $(this).parent().toggleClass("active");
+      $(this).toggleClass("rotated");
+      
+    });
+    
+   
+    $(".like").on("click",function(){
+        $(this).toggleClass("liked");              
+                  
+    }); 
+    
+
+  // Get started!
+    $(".mobile-nav-toggle").on("click",function(){
+        $(".mobile-nav").toggleClass("is-open");        
+        $(this).toggleClass("is-open");
+        
+   
+    
+    });
+    
+     $('#target').on("click",function(){
+        $('#output').html(function(i, val) { return val*1+1 });
+});
+     
+         
+ var $root = $('html, body');
+$('a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+});  
+    
+});
+
+
+$(window).scroll(function(){
+
+    youtubeVidScroll();
+    
+    
+});
+
+function youtubeVidScroll(){
+    var wScroll=$(window).scrollTop();
+    
+    $(".picture-strip").css('background-position','center -'+ wScroll/2 +'px');
+    
+
+}
+
+
+
